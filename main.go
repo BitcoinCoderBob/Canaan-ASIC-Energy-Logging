1bdcfd7c2b07ec17c64bbb0292929cfbb2f11705
BitcoinCoderBob <bitcoincoderbob@protonmail.com>
2021-09-25 21:43:11 -0400

    handle errors more gracefully

--- main.go
+++ main.go
@@ -44,29 +44,40 @@ func main() {
 		time.Sleep(time.Duration(secondsDuration) * time.Second)
 		resp, err := http.Get(url)
 		if err != nil {
-			log.Fatalln(err)
+			fmt.Printf("Error making get request to: %s with error: %s\n", url, err.Error())
+			continue
 		}
 		body, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
-			log.Fatalln(err)
+			fmt.Printf("Error reading response body: %s\n", err.Error())
+			continue
 		}
 		sb := string(body)
 		re := regexp.MustCompile(`PS\[(\d* \d* \d* \d* \d* \d*)\]`)
 		ps := re.FindString(sb)
 		lastSpaceIndex := strings.LastIndex(ps, " ")
+		if lastSpaceIndex < 0 {
+			fmt.Printf("Proper regex not found, not calculating. String: %s\n", ps)
+			continue
+		}
 		ps = ps[0:lastSpaceIndex]
 		lastSpaceIndex = strings.LastIndex(ps, " ")
+		if lastSpaceIndex < 0 {
+			fmt.Printf("Proper regex not found in second space chopping, not calculating. String: %s\n", ps)
+			continue
+		}
 		ps = ps[lastSpaceIndex+1:]
 		reading, err := strconv.ParseFloat(ps, 32)
 		if err != nil {
 			fmt.Printf("Error converting int to string. Error: %s Original string: %s", err.Error(), ps)
+			continue
 		}
 		nowTime := time.Now()
 		perpetualReadings[nowTime.String()] = reading
 		if nowTime.Sub(dayStart).Seconds() >= 86400 {
 			yesterdaysAverage := calculateAverage(dailyReadings)
 			dailyAverages[dayStart.String()] = yesterdaysAverage
-			fmt.Printf("Starting a new day. Yesterdays average was: %v\n\n", yesterdaysAverage)
+			fmt.Printf("----- Starting a new day. Yesterdays average was: %v -----\n\n", yesterdaysAverage)
 			dailyReadings = map[string]float64{}
 			dayStart = time.Now()
 		}
